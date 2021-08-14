37f86d318da46146db6ac9f3a3b9825f822caebd
tomoyuki irahara <sunnyday20200101@gmail.com>
2021-08-14 15:48:36 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TokyoComponent } from './tokyo/tokyo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TokyoComponent
   ],
   imports: [
     BrowserModule,
